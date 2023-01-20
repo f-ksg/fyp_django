3540727fc0e97a150e27a975433d7e34eaae093e
f-ksg <f-ksg@hotmail.com>
2023-01-20 22:18:21 +0800

    removed stockPredictions.apps from settings because server is constantly crashing

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stockPrediction.apps.<AppNameConfig>',
+    # 'stockPrediction.apps.<AppNameConfig>',
 ]
 
 MIDDLEWARE = [
